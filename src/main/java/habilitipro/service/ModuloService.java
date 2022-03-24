d33d8d850aa896c6f8c4e4f804c7ba7324976996
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-03-23 22:39:54 -0300

    terminando funcionalidades da camada de servico da entidade modulo

--- src/main/java/habilitipro/service/ModuloService.java
+++ src/main/java/habilitipro/service/ModuloService.java
@@ -1,13 +1,16 @@
 package habilitipro.service;
 
+import habilitipro.enums.Status;
 import habilitipro.model.dao.ModuloDAO;
-import habilitipro.model.persistence.Funcao;
+import habilitipro.model.dao.TrilhaDAO;
+import habilitipro.model.persistence.Empresa;
 import habilitipro.model.persistence.Modulo;
 import habilitipro.model.persistence.Trilha;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.persistence.*;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,10 +24,14 @@ public class ModuloService {
 
     private TrilhaService trilhaService;
 
+    private TrilhaDAO trilhaDAO;
+
     public ModuloService(EntityManager em) {
         this.em = em;
         this.moduloDAO = new ModuloDAO(em);
         this.trilhaService = new TrilhaService(em);
+        this.trilhaDAO = new TrilhaDAO(em);
+
     }
 
     public void create(Modulo modulo) {
@@ -32,6 +39,14 @@ public class ModuloService {
         validateNullModulo(modulo);
         validateDuplicate(modulo);
 
+        this.LOG.info("Verificando se já existe trilha do módulo informado...");
+        this.trilhaService.setNome(modulo.getTrilha());
+        this.trilhaService.setApelido(modulo.getTrilha());
+        Trilha trilha = this.trilhaService.findByName(modulo.getTrilha().getNome());
+
+        if(trilha != null) {
+            modulo.setTrilha(trilha);
+        }
 
         try {
             beginTransaction();
@@ -45,11 +60,55 @@ public class ModuloService {
     }
 
     public void delete(Long id) {
+        validateNullId(id);
+        this.LOG.info("Verificando se existe módulo com o Id informado...");
+        Modulo modulo = this.moduloDAO.getById(id);
+        validateNullModulo(modulo);
+        this.LOG.info("Módulo encontrado! Iniciando deleção...");
 
+        try {
+            beginTransaction();
+            this.moduloDAO.delete(modulo);
+            commitAndClose();
+        }catch (Exception e) {
+            this.LOG.error("Erro ao tentar deletar o módulo: "+e.getMessage());
+            throw new RuntimeException("Failed to delete entity Modulo");
+        }
+        this.LOG.info("Deleção realizada com sucesso!");
     }
 
     public void update(Modulo newModulo, Long moduloId) {
+        this.LOG.info("Preparando para atualizar o módulo");
+        validateNullId(moduloId);
+        validateNullModulo(newModulo);
+        this.LOG.info("Verificando se existe módulo com o id informado...");
+        Modulo modulo = this.moduloDAO.getById(moduloId);
+        validateNullModulo(modulo);
+        this.LOG.info("Módulo encontrado, iniciando atualização...");
 
+        try {
+            beginTransaction();
+            this.moduloDAO.update(modulo);
+            modulo.setPrazoLimite(newModulo.getPrazoLimite());
+            modulo.setHabilidadesTrabalhadas(newModulo.getHabilidadesTrabalhadas());
+            modulo.setNome(newModulo.getNome());
+            modulo.setTarefaDeValidacao(newModulo.getTarefaDeValidacao());
+
+            this.LOG.info("Verificando se já existe trilha do módulo informado...");
+            this.trilhaService.setNome(modulo.getTrilha());
+            this.trilhaService.setApelido(modulo.getTrilha());
+            Trilha trilha = this.trilhaService.findByName(modulo.getTrilha().getNome());
+            if(trilha != null) {
+                modulo.setTrilha(trilha);
+            }else {
+                modulo.setTrilha(newModulo.getTrilha());
+            }
+            commitAndClose();
+        }catch (Exception e) {
+            this.LOG.error("Falha ao tentar atualizar Modulo: "+e.getMessage());
+            throw new RuntimeException("Failed to update entity Modulo");
+        }
+        this.LOG.info("Atualização realizada com sucesso!");
     }
 
     public List<Modulo> listAll() {
@@ -70,6 +129,18 @@ public class ModuloService {
         validateNullList(modulos);
 
         if(modulos != null){
+            this.LOG.info(modulos.size()+" módulo(s) encontrado(s)");
+        }
+        return modulos;
+    }
+
+    public List<Modulo> listByTrilhaId(Long trilha_id) {
+        this.LOG.info("Preparando listagem de módulos pelo id da trilha...");
+        validateNullId(trilha_id);
+        List<Modulo> modulos = this.moduloDAO.listByTrilhaId(trilha_id);
+        validateNullList(modulos);
+
+        if(modulos != null) {
             this.LOG.info(modulos.size()+" módulo(s) encontrado(s)");
         }
         return modulos;
@@ -113,7 +184,7 @@ public class ModuloService {
         }
     }
 
-    private List<Modulo> validateNullList(List<Modulo> modulos) {
+    public List<Modulo> validateNullList(List<Modulo> modulos) {
         this.LOG.info("Verificando se existe registros de módulo");
         if(modulos == null) {
             this.LOG.info("Não foram encontrados módulos.");
@@ -144,6 +215,68 @@ public class ModuloService {
             this.LOG.error("Entidade módulo não encontrado");
             throw new EntityNotFoundException("Entity Modulo not found");
         }
+    }
+
+    private void setDataInicio(Modulo modulo, Status status) {
+        this.LOG.info("Verificando se o status foi definido como 'Em andamento'...");
+        if (status.equals(Status.EMANDAMENTO)) {
+            modulo.setDataInicio(OffsetDateTime.now());
+        }
+    }
+
+    private void setDataFim(Modulo modulo, Status status) {
+        this.LOG.info("Verificando se o status foi definido como 'Em fase de avaliação'...");
+         if(status.equals(Status.AVALIACAO)) {
+            modulo.setDataFim(OffsetDateTime.now());
+         }
+    }
+
+    public void setStatus(Modulo modulo, Status status) {
+        beginTransaction();
+        this.moduloDAO.update(modulo);
+        setDataInicio(modulo,status);
+        setDataFim(modulo,status);
+
+        if(modulo.getDataFim().plusDays(modulo.getPrazoLimite()).compareTo(OffsetDateTime.now())>=0){
+            this.LOG.info("Verificando se o status foi definido como 'Finalizado'...");
+            modulo.setStatus(Status.FINALIZADO);
+        }
+        commitAndClose();
+    }
+
+    public void setNivelSatisfacao(Trilha trilha, int nivelSatisfacao) {
+        final String NIVEL_SATISFACAO_TEMPLATE = "^[1-5]";
+        beginTransaction();
+        this.LOG.info("Preparando para inserir o nivel de satisfacao da trilha...");
+        trilhaDAO.update(trilha);
+
+        this.LOG.info("Listando módulos que compõem a trilha");
+        List<Modulo> modulos = listByTrilhaId(trilha.getId());
+        validateNullList(modulos);
+
+        if(modulos != null && String.valueOf(nivelSatisfacao).matches(NIVEL_SATISFACAO_TEMPLATE) &&
+                modulos.stream().filter(m->m.getStatus().equals(Status.FINALIZADO))
+                        .toArray().length== modulos.size()) {
+            trilha.setNivelSatisfacaoGeral(nivelSatisfacao);
+        }
+        commitAndClose();
+    }
+
+    public void setAnotacoes(Trilha trilha, String anotacoes) {
+        this.LOG.info("Preparando para inserir as anotações da trilha...");
+        beginTransaction();
+        trilhaDAO.update(trilha);
+
+        this.LOG.info("Listando módulos que compõem a trilha");
+        List<Modulo> modulos = listByTrilhaId(trilha.getId());
+        validateNullList(modulos);
+
+        if(modulos != null && modulos.stream().
+                filter(m->m.getStatus().equals(Status.FINALIZADO))
+                .toArray().length== modulos.size()) {
+            trilha.setAnotacoes(anotacoes);
+        }
+        commitAndClose();
     }
 
     private void beginTransaction() {
